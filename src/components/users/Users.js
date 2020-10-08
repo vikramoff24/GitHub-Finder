473ab3baa70f5884897b0791758972bb7bf0f4b4
Vikram S <vikram.senthil@outlook.com>
2020-10-08 19:08:52 +0530

    added Users Component

---
+++ src/components/users/Users.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export class Users extends Component {
+  state = {
+    users: [
+      {
+        login: "mojombo",
+        id: 1,
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      },
+      {
+        login: "defunkt",
+        id: 2,
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        html_url: "https://github.com/defunkt",
+      },
+      {
+        login: "pjhyett",
+        id: 3,
+        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+        html_url: "https://github.com/pjhyett",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.users.map((user) => (
+          <h3 key={user.id}> {user.login}</h3>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Users;
