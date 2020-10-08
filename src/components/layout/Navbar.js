8535e999ad9c542a8fb2cd564807194414df8294
Vikram S <vikram.senthil@outlook.com>
2020-10-08 20:10:39 +0530

    Update Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const NavBar = (props) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
 //default Props
-function NavBar (props){
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
-  // propType - Type Checking
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-  //for arrays Proptypes.array.isRequired
-return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={props.icon} /> {props.title}
-        </h1>
-      </nav>
-    );
-
-}
+NavBar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+// propType - Type Checking
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+//for arrays Proptypes.array.isRequired
 
 export default NavBar;
