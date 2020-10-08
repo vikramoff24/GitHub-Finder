7ed5b42b4b6e894941330375ff9aa180e0fef7a1
Vikram S <vikram.senthil@outlook.com>
2020-10-08 13:58:46 +0530

    Functional component to Class Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
