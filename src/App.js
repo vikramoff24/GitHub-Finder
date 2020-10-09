f4f8e6fae2b289c98f4d809911eada2b52c49877
Vikram S <vikram.senthil@outlook.com>
2020-10-09 16:01:13 +0530

    fetc data and mapped to component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from 'axios'; // it is used for working with apis 
 import "./App.css";
 
 class App extends Component {
+
+  state={
+     users:[],
+     loading: false
+   };
+  
+  async componentDidMount() //called when component gets mounted.
+  {
+   this.setState({loading:true});
+   const res = await axios.get("https://api.github.com/users");
+   this.setState({users:res.data, loading:false});
+   console.log(res.data);
+   
+    //axios.get("https://api.github.com/users").then(res=> console.log(res.data));
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
