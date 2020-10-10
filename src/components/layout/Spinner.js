75020262b8919ebc53c54231f2a2786401eafc3a
Vikram S <vikram.senthil@outlook.com>
2020-10-10 12:14:59 +0530

    created spinner component with gif

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React ,{Fragment} from 'react'
+import spinner from "./spinner.gif" //importing image with the help of webpack
+
+const Spinner = () => <Fragment>
+<img src ={spinner} alt ="Loading..." style={{width:'200px',margin:'auto',display:'block'}}/>
+</Fragment>
+
+export default Spinner;
