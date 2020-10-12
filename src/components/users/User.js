efa901d7a8ef7d1be34b6a688991dc0f327b1843
Vikram S <vikram.senthil@outlook.com>
2020-10-12 15:47:51 +0530

    Update User.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,16 +17,56 @@ componentDidMount()
 
     render() {
         const {name,avatar_url,location,bio,blog,login,html_url,followers,
-            following,public_repos,public_gists,hireable}=this.props.user;
+            following,public_repos,public_gists,hireable,company}=this.props.user;
 
             const {loading}=this.props;
 
 if(loading)
 {
     return <Spinner/>
-}
+} 
         return <Fragment> <Link to="/" className="btn btn-light" >Back to Search</Link>
-        Hireable:{' '}{hireable?(<i className='fas fa-check text-success'/>):(<i className='fas fa-times-circle text-danger'/>)}</Fragment>
+        Hireable:{' '}{hireable?(<i className='fas fa-check text-success'/>):(<i className='fas fa-times-circle text-danger'/>)}
+        <div className="card grid-2">
+<div className="all-center">
+<img src={avatar_url}
+className='round-img'
+alt=''
+style={{width:'150px'}}
+/>
+<h1>{name}</h1>
+<p>Location: {location}</p>
+</div>
+<div>
+    {bio&&(<Fragment>
+        <h3>Bio</h3>
+        <p>{bio}</p>
+    </Fragment>)}
+    <a href={html_url} className='btn btn-dark my-1'>
+        <i className="fab fa-github"/>{'   '}Visit Github Profile
+    </a>
+    <ul>
+       <li>{login&&(<Fragment>
+<b>Username:</b> {login}
+           </Fragment>)}
+       </li>
+       <li>{company&&(<Fragment>
+<b>Company:</b> {company}
+           </Fragment>)}</li> 
+       <li>{blog&&(<Fragment>
+<b>Website:</b> {blog}
+           </Fragment>)}</li>
+    </ul>
+</div>
+</div>
+<div className="card text-center">
+    <div className="badge badge-primary">Followers:{followers}</div>
+    <div className="badge badge-success">Following:{following}</div>
+    <div className="badge badge-light">Public Repos:{public_repos}</div>
+    <div className="badge badge-dark">Public Gists:{public_gists}</div>
+
+</div>
+          </Fragment>
         
     }
 }
