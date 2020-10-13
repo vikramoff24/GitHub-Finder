96d6a15b899c1ac34df6858f112470902a311fc6
Vikram S <vikram.senthil@outlook.com>
2020-10-13 13:02:20 +0530

    Converted App component into func and added useSate Hooks

--- src/App.js
+++ src/App.js
@@ -9,16 +9,21 @@ import Alert from "./components/layout/Alert"
 import About from "./components/pages/About"
 import "./App.css";
 
-class App extends Component {
+const App =()=> {
 
-  state={
-     users:[],
-     loading: false,
-     repos:[],
-     alert:null, 
-     user:{},
+ const [users,setUsers]=useState([]);
+ const [user,setUser]=useState({});
+ const [repos,setRepos]=useState([]);
+ const [loading,setLoading]=useState(false);
+ const [alert,setAlert]=useState(null);
+  // state={    -- this can be put into a constructor with super() called.
+  //    users:[],
+  //    loading: false,
+  //    repos:[],
+  //    alert:null, 
+  //    user:{},
     
-   };
+  //  };
 // async componentDidMount() //called when component gets mounted.
   // { 
 
@@ -28,47 +33,52 @@ class App extends Component {
   //  console.log(res.data);
   //  //axios.get("https://api.github.com/users").then(res=> console.log(res.data));
   // }
-searchUsers=async text=>
+
+const searchUsers=async text=>
 {
-  this.setState({loading:true});
+  // this.setState({loading:true});
+  setLoading(true);
   const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  this.setState({users:res.data.items, loading:false});
+  //this.setState({users:res.data.items, loading:false});
+  setUsers(res.data.items);
+  setLoading(false);
 }
 
 //Get single GitHub user
 
-getUser=async (userName)=>{
+const getUser=async (userName)=>{
+  setLoading(true);
  const res= await axios.get(`https://api.github.com/users/${userName}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
- this.setState({user:res.data, loading:false});
+ setUser(res.data);
+ setLoading(false);
 }
 
 //Get single GitHub Repos
 
-getUserRepos =async (userName)=>{
+const getUserRepos =async (userName)=>{
+  setLoading(true);
   const res= await axios.get(`https://api.github.com/users/${userName}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  this.setState({repos:res.data, loading:false});
+  //this.setState({repos:res.data, loading:false});
+  setRepos(res.data);
+  setLoading(false);
  }
 
-clearUsers=()=>
+const clearUsers=()=>
 {
-  this.setState({users:[], loading:false});
-
- 
+  setUsers([]);
+  setLoading(false);
 } 
 
-setAlert=(msg,type)=>
+const showAlert=(msg,type)=>
 {
-this.setState({alert:{
+setAlert({
   // msg:msg,
   // type:type
   // both the property and value is same 
   msg,type
-}})
-setTimeout(()=>this.setState({alert:null}),3000);
+})
+setTimeout(()=>setAlert(null),3000);
 } 
-render() {
-
-  const{users,loading,alert,user,repos}=this.state;
     return (
       <Router>
       <div className="App">
@@ -80,8 +90,8 @@ render() {
         {/* Routing the site to home page */}
         <Route exact path='/' render ={(props)=>(
           <Fragment>
-        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
-            this.state.users.length>0?true:false} setAlert={this.setAlert}/>
+        <Search searchUsers={searchUsers} clearUsers={clearUsers} showClear={
+            users.length>0?true:false} setAlert={showAlert}/>
           <Users loading={loading} users={users}    />
           </Fragment>
         )} />
@@ -91,7 +101,7 @@ render() {
        <Route exact path="/about" component={About}/>
        {/* Routing to single User component */}
        <Route exact path="/user/:login" render={(props)=><User {...props}
-        getUser={this.getUser} user={user} loading={loading} getUserRepos={this.getUserRepos}
+        getUser={getUser} user={user} loading={loading} getUserRepos={getUserRepos}
         repos={repos} />
         }/>
 </Switch>
@@ -100,6 +110,5 @@ render() {
       </Router>
     );
   }
-}
 
 export default App;
