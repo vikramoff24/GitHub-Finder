c29fa60f39fb86f5014f74366d9235bcb3b7209c
Vikram S <vikram.senthil@outlook.com>
2020-10-15 20:40:43 +0530

    Update AlertState.js

---
+++ src/components/context/alert/AlertState.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AlertContext from './alertContext'
+import AlertReducer from './alertReducer'
+import {SET_ALERT,REMOVE_ALERT} from "../types"
+
+const alertState=()=>
+{
+
+}
+
+
+
+const [state, dispatch] = useReducer(AertReducer, initialState)
+
